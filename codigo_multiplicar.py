9fa2aa7b3ba9f8a75600d428e4c6e3796658634a
maria cristina victoria <gillargomariacristina@gmail.com>
2023-06-13 20:32:53 +0200

    agregar codigo multiplicar

---
+++ codigo_multiplicar.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 20:27:53 2023
+
+@author: Alumno
+"""
+
+# Pedir al usuario dos números
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+# Multiplicar los dos números
+resultado = num1 * num2
+
+# Imprimir el resultado
+print("El resultado de la multiplicación es:", resultado)
